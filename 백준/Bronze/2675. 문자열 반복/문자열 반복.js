d3a229f76fbe3b7c9c14d31d54e20524de15c7c5
minukHwang <104661043+minukHwang@users.noreply.github.com>
2024-07-20 20:48:23 +0900

    [Bronze II] Title: 문자열 반복, Time: 136 ms, Memory: 9460 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.js
@@ -0,0 +1,37 @@
+const rl = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let N;
+let input = [];
+let answer = '';
+let count = 0;
+
+rl.on('line', (line) => {
+    if(count === 0){
+        N = Number(line);
+        count++;
+    } else {
+        input.push(line.split(' '));
+        if(count === N) rl.close();
+
+        count++;
+    }
+}).on('close', () => {
+    input.forEach((item) => {
+        const cnt = item[0];
+
+        for(let letter of item[1]){
+            for(let i = 0; i < cnt; i++){
+                answer += letter;
+            }
+        }
+
+        answer += '\n';
+    })
+
+    console.log(answer);
+    process.exit(0);
+});
+
