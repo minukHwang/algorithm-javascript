ae6a56d0ad84b013dbb52de72a8ae37a25595e3c
minukHwang <104661043+minukHwang@users.noreply.github.com>
2024-07-20 23:49:59 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 620 ms, Memory: 62176 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,36 @@
+const rl = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let N;
+let coords = [];
+let lineCount = 0;
+let answer = '';
+
+const compare = (a, b) => {
+    if(a.y === b.y) return a.x - b.x;
+    return a.y - b.y;
+}
+
+rl.on('line', (line) => {
+    if(lineCount === 0){
+        N = Number(line);
+        lineCount++;
+    } else {
+        const[x, y] = line.split(' ').map(Number);
+        coords.push({x, y});
+
+        if(lineCount === N) rl.close();
+
+        lineCount++;
+    }
+}).on('close', () => {
+    coords.sort(compare);
+
+    coords.forEach((coord) => {
+        answer += `${coord.x} ${coord.y}\n`;
+    })
+
+    console.log(answer);
+});
