2c028f88f3fe4e80b5eb2e0ac85a75e6d0880022
minukHwang <104661043+minukHwang@users.noreply.github.com>
2025-07-16 22:22:14 +0900

    [Silver II] Title: 예산, Time: 156 ms, Memory: 10156 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.js
+++ 백준/Silver/2512. 예산/예산.js
@@ -2,27 +2,31 @@ let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
 let N = Number(input[0]);
-let requests = input[1].split(' ').map(Number);
+let arr = input[1].split(' ').map(Number);
 let M = Number(input[2]);
 
-let start = 1;
-let end = requests.reduce((a, b) => Math.max(a, b));
+let first = 1;
+let last = arr.reduce((a, b) => Math.max(a, b));
 
 let result = 0;
-while (start <= end) {
-  let mid = parseInt((start + end) / 2);
+
+while (last >= first) {
+  let mid = parseInt((first + last) / 2);
+
   let total = 0;
-  for (x of requests) {
-    total += Math.min(mid, x);
+  for (let i = 0; i < N; i++) {
+    total += Math.min(mid, arr[i]);
+  }
+
+  if (total > M) {
+    last = mid - 1;
   }
 
   if (total <= M) {
     result = mid;
-    start = mid + 1;
+    first = mid + 1;
   }
-  else {
-    end = mid - 1;
-  }
+
 }
 
 console.log(result);
