8e89f32d7a5b5aa849bca3d975cd9fc45a777974
minukHwang <104661043+minukHwang@users.noreply.github.com>
2025-08-06 22:38:47 +0900

    [Silver III] Title: N과 M (2), Time: 92 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -0,0 +1,32 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ').map(Number);
+
+let N = input[0];
+let M = input[1];
+
+let arr = [];
+for (let i = 1; i <= N; i++) arr.push(i);
+let visited = Array(N).fill(false);
+let selected = [];
+
+let answer = '';
+function dfs(start, depth) {
+  if (depth === M) {
+    for (x of selected) answer += x + ' ';
+    answer += '\n';
+    return;
+  }
+
+  for (let i = start; i < N; i++) {
+    if (!visited[i]) {
+      selected.push(arr[i]);
+      visited[i] = true;
+      dfs(i + 1, depth + 1);
+      selected.pop();
+      visited[i] = false;
+    }
+  }
+}
+
+dfs(0, 0);
+console.log(answer);
